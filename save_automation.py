25db1129a34ddd3126e5464cac5e48c3c04e93d4
OJHacker <ojhacker.master@gmail.com>
2024-11-17 12:01:18 +0530

    An automation script for saving any files
    An automation script that will help user to save the file automatically, when
    the editor is not supported for auto-save.

---
+++ save_automation.py
@@ -0,0 +1,18 @@
+import schedule
+import time
+import pyautogui
+
+
+def job():
+    pyautogui.hotkey('ctrl', 's')
+    print("Saved")
+
+# Schedule task every 10 seconds
+schedule.every(10).seconds.do(job)
+
+try:
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Scheduler stopped.")
